2fbbc7c91366634d96f7da4f6bbbf816630d99b4
Bart Saelen <bart.saelen@geosolutions.be>
2018-02-20 09:59:33 +0100

    Autowired

--- src/main/java/be/project28/didemo/DiDemoApplication.java
+++ src/main/java/be/project28/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package be.project28.didemo;
 
+import be.project28.didemo.controllers.ContructorInjectedController;
 import be.project28.didemo.controllers.MyController;
+import be.project28.didemo.controllers.PropertyInjectedController;
+import be.project28.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +17,9 @@ public class DiDemoApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 
 		myController.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ContructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/be/project28/didemo/controllers/ContructorInjectedController.java
+++ src/main/java/be/project28/didemo/controllers/ContructorInjectedController.java
@@ -1,16 +1,20 @@
 package be.project28.didemo.controllers;
 
 import be.project28.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ContructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public ContructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
